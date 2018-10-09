31bad8c4494cafc80a3e28033d604a5bd04bb32f
thomas <tngo0508@gmail.com>
2018-10-09 01:28:29 -0700

    make button interactive

--- app/src/main/java/edu/fullerton/cpsc411/myfirstapp/MainActivity.kt
+++ app/src/main/java/edu/fullerton/cpsc411/myfirstapp/MainActivity.kt
@@ -2,11 +2,35 @@ package edu.fullerton.cpsc411.myfirstapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe(view: View) {
+        // val myToast = Toast.makeText(this, message, duration);
+        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
+    }
+
+    fun countMe(view: View) {
+        // Get the text view
+        val showCountView = findViewById(R.id.textView) as TextView
+
+        // Get the value of the text view
+        val countString = showCountView.text.toString()
+
+        // Convert value to a number and increment it
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        // Display the new value in the text view.
+        showCountView.text = count.toString()
     }
 }
